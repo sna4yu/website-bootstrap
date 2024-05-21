96332e440ca2f8677599d0ccc5330b56ee19a13d
sna4yu <mrveasna43@gmail.com>
2024-05-21 12:36:43 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  $("#login").click(function () {
+    $("#form").removeClass("d-none");
+  });
+  $("#x").click(function () {
+    $("#form").addClass("d-none");
+  });
+  $("#login1").click(function () {
+    $("#form").removeClass("d-none");
+    $("#newform").addClass("d-none");
+  });
+  $("#x").click(function () {
+    $("#form").addClass("d-none");
+  });
+  $("#login2").click(function () {
+    $("#form").removeClass("d-none");
+    $("#newform1").addClass("d-none");
+  });
+  $("#x").click(function () {
+    $("#form").addClass("d-none");
+  });
+
+  $("#create").click(function () {
+    $("#newform").removeClass("d-none");
+    $("#form").addClass("d-none");
+  });
+  $("#newx").click(function () {
+    $("#newform").addClass("d-none");
+  });
+
+  $("#forgot").click(function () {
+    $("#newform1").removeClass("d-none");
+    $("#form").addClass("d-none");
+  });
+  $("#newx1").click(function () {
+    $("#newform1").addClass("d-none");
+  });
+
+  $("#search").click(function () {
+    $("#search1").removeClass("d-none");
+  });
+  $("#x1").click(function () {
+    $("#search1").addClass("d-none");
+  });
+  $("#buy").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy1").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy2").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy3").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy4").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy5").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy6").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+  $("#buy7").click(function () {
+    $("#buyform").removeClass("d-none");
+  });
+  $("#k").click(function () {
+    $("#buyform").addClass("d-none");
+  });
+});
